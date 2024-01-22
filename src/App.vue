4ea050d8f3e3b56a1cc7171838c5b8bde573ffd2
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2024-01-22 08:09:06 +0000

    fist commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>Pedro Henrique</h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-evenly;
+    justify-content: center;
+  }
+</style>
